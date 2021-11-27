2ce9a94c8ba37879e38ae5eec7aa6dd39222c7d1
pa_romano <pierantonioromano@gmail.com>
2021-11-27 23:50:09 +0100

    404 fix

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ export default function Custom404() {
 			<div className="w-96 bg-white bg-opacity-70 p-0 rounded-lg">
 				<h1 className="inline-block text-2xl font-bold border-r border-gray-400 p-2 pr-4 m-4 mr-0 align-middle">404</h1>
 				<span className="inline-block text-sm p-2 pl-4 m-4 ml-0 align-middle">Ooops, abbiamo un problema!<br/>La pagina è stata spostata o rimossa.</span>
-				<a className="p-4 block w-full bg-opacity-95 bg-blue-500 hover:bg-blue-600 text-center text-white rounded-b-lg font-bold" href="/">Torna alla home page <ArrowSmRightIcon className="w-4 inline ml-1" /></a>
+				<a className="p-4 block w-full bg-opacity-95 bg-blue-500 hover:bg-blue-600 text-center text-white rounded-b-lg font-bold" href="/">Torna alla home page <ArrowSmRightIcon className="w-4 inline ml-1" /></a> 
 			</div>
 		</div>
 	)
